0259e7298dcb73b49fe773f4b42a79b730707f20
Young <fuyangzhen@gmail.com>
2023-08-06 11:03:29 +0800

    update with PWA

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,11 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const docsPath = "../docs/";
 
 module.exports = {
   entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, "../docs/"),
+    path: path.resolve(__dirname, docsPath),
     filename: "bundle.js",
   },
   module: {
@@ -21,6 +23,13 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/sw.js", to: docsPath },
+        { from: "src/icon512.png", to: docsPath },
+        { from: "src/manifest.json", to: docsPath },
+      ],
     }),
   ],
 };
